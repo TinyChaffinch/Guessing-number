06c4b1e82e8982316363ce9d655b88b0359b4a06
Dmitry <DvoroninD@yandex.ru>
2022-05-23 14:47:19 +0300

    add timer, comments + some fix

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let startNum = document.querySelector('.startNum');
 let endNum = document.querySelector('.endNum');
 let startEndBut = document.querySelector('.startEndBut');
+
+let timer = document.querySelector('.timer');
+let timecheck = document.querySelector('.timecheck');
+
 let guesses = document.querySelector('.guesses');
 let trys = document.querySelector('.trys');
 let lastResult = document.querySelector('.lastResult');
@@ -14,18 +18,45 @@ let randomNumber;
 let guessCount = 1;
 let resetButton;
 
+let timeCounter;
+
 guessField.disabled = true;
 guessSubmit.disabled = true;
 guessField.focus();
+
+//Функция RGB палитры
 
 function rgb(r, g, b) {
     return 'rgb(' + [(r || 0), (g || 0), (b || 0)].join(',') + ')';
 }
 
+//Функция таймера
+
+function timerCount() {
+    let i = 61;
+    timeCounter = setInterval(
+        () => {
+            i--;
+            timer.textContent = 'Осталось: ' + i + ' секунд';
+            if (i === 0) {
+                clearInterval(timeCounter);
+                timer.textContent = 'Таймер ';
+                alert('Время вышло');
+                setGameOver();
+            }
+        },
+        1000)
+}
+
+//Функция начала игры
+
 function startGame() {
-    if (Number(endNum.value)-Number(startNum.value) < 20) {
+    if (Number(endNum.value) - Number(startNum.value) < 20) {
         alert('Вы ввели некоректные данные');
     } else {
+        if (timecheck.checked) {
+            timerCount();
+        }
         let startRand = Number(startNum.value);
         let endRand = Number(endNum.value);
         randomNumber = Math.floor(Math.random() * (startRand - endRand + 1)) + endRand;
@@ -39,6 +70,8 @@ function startGame() {
 
 startEndBut.addEventListener('click', startGame);
 
+//Функция проверки ответа
+
 function checkGuess() {
     let userGuess = Number(guessField.value);
     if (guessCount === 1) {
@@ -50,6 +83,7 @@ function checkGuess() {
         lastResult.style.color = rgb(51, 255, 51);
         lastResult.textContent = 'Поздравляю! Вы угадали число!';
         lowOrHi.textContent = '';
+        clearInterval(timeCounter);
         setGameOver();
     } else if (guessCount === 10) {
         lastResult.textContent = '!!!ИГРА ОКОНЧЕНА!!!';
@@ -74,6 +108,8 @@ function checkGuess() {
 
 guessSubmit.addEventListener('click', checkGuess);
 
+//Функция создания копки новой игры и её логика
+
 function setGameOver() {
     guessField.disabled = true;
     guessSubmit.disabled = true;
@@ -86,8 +122,15 @@ function setGameOver() {
 
 function resetGame() {
     guessCount = 1;
-
     var resetParas = document.querySelectorAll('.resultParas p');
+
+    var newcheckbox = document.createElement('input');
+    newcheckbox.setAttribute('type', 'checkbox');
+    newcheckbox.setAttribute('class', 'timecheck')
+    timer.textContent = 'Таймер ';
+    timer.appendChild(newcheckbox);
+    timecheck = document.querySelector('.timecheck');
+
     for (var i = 0; i < resetParas.length; i++) {
         resetParas[i].textContent = '';
     }
@@ -105,6 +148,8 @@ function resetGame() {
     randomNumber = Math.floor(Math.random() * 100) + 1;
 }
 
+//Проверки на ввод
+
 const changeHandler = e => {
     const value = e.value;
     e.value = value.replace(/\D/g, '');
@@ -117,10 +162,10 @@ function isright(obj) {
     if (obj.value > Number(endNum.value)) {
         obj.value = Number(endNum.value);
     }
-    if (obj.value < startNum.value) {
+    if (obj.value < Number(startNum.value)) {
         guessSubmit.disabled = true;
     }
-    if (obj.value >= startNum.value) {
+    if (obj.value >= Number(startNum.value)) {
         guessSubmit.disabled = false;
     }
 }
